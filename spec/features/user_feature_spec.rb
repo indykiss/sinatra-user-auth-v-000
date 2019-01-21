591d9611751bc9663ab7a7b4b99a409bd7938dbd
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-01-21 17:23:26 +0000

    Automatically backed up by Learn

--- spec/features/user_feature_spec.rb
+++ spec/features/user_feature_spec.rb
@@ -39,7 +39,7 @@ describe "User sign up, log in, sign out" do
     end
 
 
-#Sorry- I commented out a spec here. There's no codealong for the incorrect password path. 
+#Sorry- I commented out a spec here. There's no codealong for the incorrect password path. Worked on this for 2 hrs before figuring that out. 
 #Can't figure it out. Will try again in next lab.
    # it 'fails to log in with an incorrect password' do
    #   expect(current_path).to eq('/sessions/login')
