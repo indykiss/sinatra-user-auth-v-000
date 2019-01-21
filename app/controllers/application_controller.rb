a51d62da6235bc1272dae8d8ad735fe21a7cfd07
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-01-21 17:16:13 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -36,7 +36,7 @@ class ApplicationController < Sinatra::Base
     end 
     # Should an if statement go here?
     session[:id] = @user.id
-    redirect '/sessions/login'
+    redirect '/sessions'
   end
 
   get '/sessions/logout' do 
